242b1068a0379a759cdec16823ca65fd625f0555
kylehue <apeuda69@gmail.com>
2021-07-03 15:59:20 +0800

    Update

--- src/client/js/vue/vue.js
+++ src/client/js/vue/vue.js
@@ -1,7 +1,9 @@
 const timeline = require("./timeline.js");
+const contextMenu = require("./contextMenu.js");
 const apps = {
 	timeline: timeline,
-	toolApp: document.getElementById("toolApp")
+	toolApp: document.getElementById("toolApp"),
+	contextMenuApp: contextMenu
 };
 
 module.exports = apps;
