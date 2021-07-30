dadc82fad9a4025cfaecccac06a593f8f5ced19b
kylehue <apeuda69@gmail.com>
2021-07-30 10:57:07 +0800

    Update 2021-07-30T02:57:03.977Z

--- server/server.js
+++
@@ -1,8 +0,0 @@
-//Setup server
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
-});
-app.use(express.static(__dirname + "/../client"));
